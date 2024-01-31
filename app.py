bb04c02a60a9710665b4a80d7047cdbfca499fa3
Asror Bobomurodov <139123057+asrorbobomurodov@users.noreply.github.com>
2024-01-31 12:44:57 +0500

    done

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import telegram
+
+Token = '6365601102:AAFCK2wKjgM3Ui88ao5YQFokIQePu98B1GE'
+bot = telegram.Bot(Token)
+chat_id = 6082673969
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    bot.send_message(chat_id=chat_id, text = "Salom Asror")
     return "Home page, Asror"
+
+if __name__=="__main__":
+    app.run(debug=True)
